feb26c4bec60932d165d916e52b6767ea8ba0fb2
vhuynh192k2 <huynh.vo@digitalts.co>
2022-10-09 23:32:45 +0700

    fix css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,17 @@ import Head from "next/head";
 
 import "swiper/css/bundle";
 import "antd/dist/antd.css";
+import "aos/dist/aos.css";
 
 import "../styles/bootstrap.min.css";
 import "../styles/animate.css";
 import "../styles/meanmenu.css";
-import "../styles/boxicons.min.css";
 import "../styles/flaticon.css";
 import "../styles/style.css";
 import "../styles/responsive.css";
-import "aos/dist/aos.css";
 
 import type { AppProps } from "next/app";
 import { AppLayout } from "@layouts";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faScroll,
@@ -82,12 +80,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Arapey:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          type="text/css"
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         />
       </Head>
